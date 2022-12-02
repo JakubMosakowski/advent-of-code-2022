8463457a9b68c26ed4158da69de8e9f52fc64572
jakubmosakowski <jmosakowski@spoton.com>
2022-12-02 11:28:34 +0100

    Add part2 to day1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -12,6 +12,7 @@ import readInput
  * Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar;
  * the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
  *
+ * PART 1:
  * The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the Elves' expedition traditionally goes on foot.
  * As your boats approach land, the Elves begin taking inventory of their supplies. One important consideration is food - in particular, the number of Calories each Elf is carrying (your puzzle input).
  *
@@ -45,6 +46,18 @@ import readInput
  * In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
  *
  * Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+ *
+ *
+ * PART 2:
+ * By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
+ *
+ * To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories.
+ * That way, even if one of those Elves runs out of snacks, they still have two backups.
+ *
+ * In the example above, the top three Elves are the fourth Elf (with 24000 Calories), then the third Elf (with 11000 Calories), then the fifth Elf (with 10000 Calories).
+ * The sum of the Calories carried by these three elves is 45000.
+ *
+ * Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
  */
 fun main() {
     fun part1(input: List<String>): Int {
