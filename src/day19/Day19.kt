049734c9c7d457d5bbbca6acadf3941b5526be10
jakubmosakowski <jmosakowski@spoton.com>
2022-12-09 18:30:38 +0100

    Add template for all other days

---
+++ src/day19/Day19.kt
@@ -0,0 +1,26 @@
+package day19
+
+import readInput
+
+/**
+ *
+ *
+ * PART 2:
+ *
+ */
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val testInput = readInput("test_input")
+    check(part1(testInput) == 24000)
+
+    val input = readInput("input")
+    println(part1(input))
+    println(part2(input))
+}
